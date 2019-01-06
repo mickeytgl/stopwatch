57a3b8c0c8124b00f18caaedc964fdf70f28bb78
Miguel Torres <migueltg93@gmail.com>
2019-01-06 16:42:39 -0600

    Simple stopwatch timer on React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let model = {
+  time: 0,
+  running: false,
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const intent = {
+  TICK: 'TICK',
+  START: 'START',
+  STOP: 'STOP',
+  RESET: 'RESET'
+}
+
+const view = (m) => {
+  let minutes = Math.floor(model.time / 60);
+  let seconds = model.time - (minutes * 60);
+  let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`
+  let handler = (event) => {
+    model = update(model, m.running ? 'STOP' : 'START')
+  }
+  return <div>
+    <p>{minutes}:{secondsFormatted}</p>
+    <button onClick={handler}>{m.running ? 'Stop' : 'Start'}</button>
+  </div>
+}
+
+const update = (model, intent) => {
+  const updates = {
+    'TICK': (model) => Object.assign(model, {time: model.time + (model.running ? 1 : 0)}),
+    'RESET': (model) => Object.assign(model, {time: 0}),
+    'START': (model) => Object.assign(model, {running: true}),
+    'STOP': (model) => Object.assign(model, {running: false})
+  };
+  return updates[intent](model)
+}
+
+const render = () => {
+  ReactDOM.render(view(model),
+  document.getElementById('root')
+  );
+};
+render();
+
+setInterval(() => {
+  model = update(model, 'TICK');
+  render();
+}, 1000);
