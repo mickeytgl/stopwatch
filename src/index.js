54945bb7cf3d12a56ab548fb47d90b551bef6069
Miguel Torres <migueltg93@gmail.com>
2019-01-23 09:36:06 -0600

    Used Redux for state container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
 let model = {
   time: 0,
@@ -16,11 +16,11 @@ const intent = {
 }
 
 const view = (m) => {
-  let minutes = Math.floor(model.time / 60);
-  let seconds = model.time - (minutes * 60);
+  let minutes = Math.floor(m.time / 60);
+  let seconds = m.time - (minutes * 60);
   let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`
   let handler = (event) => {
-    model = update(model, m.running ? 'STOP' : 'START')
+    container.dispatch(m.running ? {type: 'STOP'} : {type: 'START'})
   }
   return <div>
     <p>{minutes}:{secondsFormatted}</p>
@@ -28,24 +28,25 @@ const view = (m) => {
   </div>
 }
 
-const update = (model, intent) => {
+const update = (model = { running: false, time: 0 }, action) => {
   const updates = {
     'TICK': (model) => Object.assign(model, {time: model.time + (model.running ? 1 : 0)}),
     'RESET': (model) => Object.assign(model, {time: 0}),
     'START': (model) => Object.assign(model, {running: true}),
     'STOP': (model) => Object.assign(model, {running: false})
   };
-  return updates[intent](model)
+  return (updates[action.type] || (() => model))(model);
 }
 
+let container = createStore(update);
+
 const render = () => {
-  ReactDOM.render(view(model),
+  ReactDOM.render(view(container.getState()),
   document.getElementById('root')
   );
 };
-render();
+container.subscribe(render);
 
 setInterval(() => {
-  model = update(model, 'TICK');
-  render();
+  container.dispatch({type: 'TICK'});
 }, 1000);
